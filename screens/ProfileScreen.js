844cf326ece7a92e5dfc35f5b48efca9b1b1c81d
Lorenzo <lorenzopalumbo.dev@gmail.com>
2022-12-23 13:29:31 +0100

    Setup

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function Profile() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start on your app!</Text>
+      <Text>Profile</Text>
       <StatusBar style="auto" />
     </View>
   );
