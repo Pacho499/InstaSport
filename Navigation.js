0339844531e714dec871d44c351db69e5f292c35
Lorenzo <lorenzopalumbo.dev@gmail.com>
2022-12-24 14:12:54 +0100

    drawer navigation

--- Navigation.js
+++ Navigation.js
@@ -1,23 +1,34 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from './screens/HomeScreen'
 import PostDetail from './screens/PostDetailScreen'
 import Profile from './screens/ProfileScreen'
 import SavedPost from './screens/SavedPostScreen'
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator()
 
-function Navigation() {
+function StackNavigation() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="PostDetail" component={PostDetail} />
         <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="SavedPost" component={SavedPost} />
       </Stack.Navigator>
-    </NavigationContainer>
   );
 }
 
-export default Navigation;
+function DrawerNavigation () {
+    return(
+        <NavigationContainer>
+            <Drawer.Navigator>
+                <Drawer.Screen name='StackNavigation' component={StackNavigation}/>
+                <Drawer.Screen name='SavedPost' component={SavedPost}/>
+            </Drawer.Navigator>
+        </NavigationContainer>  
+    )
+   
+}
+
+export default DrawerNavigation;
