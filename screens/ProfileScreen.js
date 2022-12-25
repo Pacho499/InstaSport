fd59f5a565b7d826a136a96dc1e3da489da5a38c
Lorenzo <lorenzopalumbo.dev@gmail.com>
2022-12-25 23:27:54 +0100

    profileScreen done

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,10 +1,12 @@
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, ImageBackground, Text } from 'react-native';
 import {SCREEN_HEIGHT, SCREEN_WIDTH} from '../utils/helper'
 import Data from '../data/fake-data'
 
 export default function Profile(props) {
   const {userId} = props.route.params
   const userPosts = Data.filter(post => post.userId === userId)
+  const userImage = userPosts[0].userImage
+  const userName = userPosts[0].userName
   const postsImages = userPosts.map(post => {
     return (
       <Image source={{uri: post.image}} style={styles.image} />
@@ -12,7 +14,16 @@ export default function Profile(props) {
   })
   return (
     <View style={styles.container}>
-      {postsImages}
+      <View>
+        <ImageBackground source={{uri: userImage}} blurRadius={10} style={styles.imageBackground}  >
+            <Image source={{uri: userImage}} style={styles.userImage} /> 
+        </ImageBackground>
+      </View>
+      <Text style={{marginTop:50, fontSize: 20, fontWeight: '600', alignSelf:'center'}}>{userName}</Text>
+      <View style={styles.postContainer}>
+        {postsImages}
+      </View>
+     
     </View>
   );
 }
@@ -21,13 +32,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    flexWrap: 'wrap'
   },
   image: {
     width: SCREEN_WIDTH / 3,
     height: SCREEN_HEIGHT / 3
+  }, 
+  postContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap', 
+    marginTop: 50 ,
+  }, 
+  userImage: {
+    height: 50, 
+    width: 50 , 
+    borderRadius: 50,
+    justifyContent:'center',
+    position: 'absolute', 
+    top: 75 
+  }, 
+  imageBackground: {
+    width: SCREEN_WIDTH, 
+    height: 100, 
+    alignItems:'center'
   }
 });
